781828549167f0f3ce838e46c82e1531e0ca51d2
st3meer <rdudelis88@gmail.com>
2025-03-05 11:25:18 +0200

    skeleton ready

---
+++ findafriend1/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <AppHeader />
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+
+  //
+</script>
---
+++ findafriend1/src/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-bottom-navigation grow>
+
+  <v-btn value="Map">
+    <v-icon>mdi-map-marker</v-icon>
+
+    <span>Map</span>
+  </v-btn>
+
+  <v-btn value="Friends">
+    <v-icon>mdi-account-group</v-icon>
+
+    <span>Friends</span>
+  </v-btn>
+
+  <v-btn value="Events">
+    <v-icon>mdi-star-box</v-icon>
+
+    <span>Events</span>
+  </v-btn>
+
+  <v-btn value="Chats">
+    <v-icon>mdi-chat</v-icon>
+
+    <span>Chats</span>
+  </v-btn>
+</v-bottom-navigation>
+</template>
+
+<script setup>
+  
+</script>
